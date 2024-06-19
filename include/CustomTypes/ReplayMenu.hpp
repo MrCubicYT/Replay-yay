caa72d87222250158c58ec0ee7739ec1eec4b773
Cubic <132150361+MrCubicYT@users.noreply.github.com>
2024-06-19 22:40:31 +0100

    Update ReplayMenu.hpp

--- include/CustomTypes/ReplayMenu.hpp
+++ include/CustomTypes/ReplayMenu.hpp
@@ -1,41 +1,41 @@
-#pragma once
+#pragma once //make sure reffered to once
 
-#include "GlobalNamespace/LevelBar.hpp"
+#include "GlobalNamespace/LevelBar.hpp" //game stuff
 #include "GlobalNamespace/StandardLevelDetailView.hpp"
 #include "GlobalNamespace/IReadonlyBeatmapData.hpp"
 #include "HMUI/ViewController.hpp"
 #include "HMUI/ModalView.hpp"
 #include "HMUI/SimpleTextDropdown.hpp"
 #include "TMPro/TextMeshProUGUI.hpp"
-#include "UnityEngine/UI/Button.hpp"
+#include "UnityEngine/UI/Button.hpp" //game stuff
 
 #include "questui/shared/CustomTypes/Components/Settings/IncrementSetting.hpp"
 
 #include "custom-types/shared/macros.hpp"
 
-struct ReplayInfo;
+struct ReplayInfo; //declare struct
 
-namespace Menu {
-    void EnsureSetup(GlobalNamespace::StandardLevelDetailView* view);
+namespace Menu { //new menu namespace
+    void EnsureSetup(GlobalNamespace::StandardLevelDetailView* view); //declare func
 
-    void SetButtonEnabled(bool enabled);
-    void SetButtonUninteractable(std::string_view reason);
+    void SetButtonEnabled(bool enabled); //declare func
+    void SetButtonUninteractable(std::string_view reason); //declare func
 
-    void CheckMultiplayer();
+    void CheckMultiplayer(); //declare func
 
-    void SetReplays(std::vector<std::pair<std::string, ReplayInfo*>> replays, bool external = false);
+    void SetReplays(std::vector<std::pair<std::string, ReplayInfo*>> replays, bool external = false); //declare func
 
-    void PresentMenu();
-    void DismissMenu();
+    void PresentMenu(); //declare func
+    void DismissMenu(); //declare func
 
-    bool AreReplaysLocal();
+    bool AreReplaysLocal(); //declare func
 }
 
 DECLARE_CLASS_CODEGEN(Menu, ReplayViewController, HMUI::ViewController,
 
     DECLARE_OVERRIDE_METHOD(void, DidActivate,
         il2cpp_utils::il2cpp_type_check::MetadataGetter<&HMUI::ViewController::DidActivate>::get(),
-        bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
+        bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling); //overide smthing with menu namespace?
 
     DECLARE_INSTANCE_METHOD(void, UpdateUI);
     DECLARE_INSTANCE_METHOD(void, OnEnable);
